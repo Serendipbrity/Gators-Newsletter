d742007e7c770bfd275abcb70ca7c72fd51c6dfd
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-04-05 21:05:29 -0400

    date

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var current = document.querySelector("#currentDay");
+
+let date = new Date()
+let day = date.getDate();
+let month = date.getMonth()  + 1;
+let year = date.getFullYear();
+
+let fullDate = `${month}.${day}.${year}`;
+console.log("Full date: " + fullDate);
+
+current.innerHTML = "Date: " + fullDate;
